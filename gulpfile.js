202527f4219d376920fa4dfdf9f7a45f338c57b8
alehdezc <alejandro.hcolmenares@hotmail.com>
2024-04-12 00:04:39 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 function css(done) {  
-    src('src/scss/app.scss') // Identificar el archivo SASS
-        .pie( sass() ) // Compilarlo
+    src("src/scss/app.scss") // Identificar el archivo SASS
+        .pipe( sass()) // Compilarlo
         .pipe( dest("build/css")); // Almacenarla en el disco duro
 
 
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function dev(done) {
+    watch("src/scss/app.scss", css)
+
+
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
